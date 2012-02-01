6fbdd25053dadb19c69b3d672d86ddaa27fc8199
José García V <jose3500@gmail.com>
2012-02-01 16:35:32 -0600

    Actualizando los Archivos README

--- Python_HH/ABCM_Python/main.py
+++ Python_HH/ABCM_Python/main.py
@@ -1,5 +1,13 @@
 import Empleado
 
+print "Seleccione una Opcion:\n"
+print "---------------------------------"
+print "1.- Alta de Empleado"
+print "2.- Baja de Empleado"
+print "3.- Consulta de Empleado"
+print "4.- Modificar de Empleado"
+print "5.- SALIR"
+print "---------------------------------"
 
 a = Empleado.Empleado("Jose","Garcia Vargas","Ingeniero Mecatronica","4621104829")
 a.Imprimir_Personal()
