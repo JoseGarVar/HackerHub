165fbd95651009a95a99ef0518b76b330569a998
José García V <jose3500@gmail.com>
2012-02-02 19:35:04 -0600

    Modificaciones Menores

--- Python_HH/ABCM_Python/main.py
+++ Python_HH/ABCM_Python/main.py
@@ -1,30 +1,34 @@
 import Empleado
 
-print "Seleccione una Opcion:\n"
-print "---------------------------------"
-print "1.- Alta de Empleado"
-print "2.- Baja de Empleado"
-print "3.- Consulta de Empleado"
-print "4.- Modificar Empleado"
-print "5.- SALIR"
-op = int(raw_input(": "))
-print "---------------------------------"
+op = 1
 
-#Opcion = { 1 : "Alta de Empleado" , 2 : "Baja de Empleado" , 3 : "Consulta de Empleado" , 4 : "Modificar Empleado"}
+while(op <> 5):
+    
+    print "Seleccione una Opcion:\n"
+    print "---------------------------------"
+    print "1.- Alta de Empleado"
+    print "2.- Baja de Empleado"
+    print "3.- Consulta de Empleado"
+    print "4.- Modificar Empleado"
+    print "5.- SALIR"
+    op = int(raw_input(": "))
+    print "---------------------------------"
 
-MAX_EMP = 100
-Emp = Empleado.Empleado()
-#Emp.Imprimir_Personal()
-#a = Empleado.Empleado("Jose","Garcia Vargas","Ingeniero Mecatronica","4621104829","001")
-#a.Imprimir_Personal()
-#print Opcion[op]
+    #Opcion = { 1 : "Alta de Empleado" , 2 : "Baja de Empleado" , 3 : "Consulta de Empleado" , 4 : "Modificar Empleado"}
 
-if op == 1:    
-    Numero = raw_input("Ingrese el Numero del Trabajador: ")
-    Nombre = raw_input("Ingrese el Nombre del Trabajador: ")
-    Apellidos = raw_input("Ingrese los Apellidos del Trabajador: ")
-    Profesion = raw_input("Ingrese la Profesion del Trabajador: ")
-    Telefono = raw_input("Ingrese el Telefono del Trabajador: ")
-    Emp.Alta("001","Jose","Garcia Vargas","Ingeniero Mecatronica","4621104829")
-    #Emp = Empleado.Alta(Numero,Nombre,Apellidos,Profesion,Telefono)
+    MAX_EMP = 100
+    Numero = 1
+    Emp = Empleado.Empleado()
+    #Emp.Imprimir_Personal()
+    #a = Empleado.Empleado("Jose","Garcia Vargas","Ingeniero Mecatronica","4621104829","001")
+    #a.Imprimir_Personal()
+    #print Opcion[op]
+
+    if op == 1:    
+        Numero = raw_input("Ingrese el Numero del Trabajador: ")
+        Nombre = raw_input("Ingrese el Nombre del Trabajador: ")
+        Apellidos = raw_input("Ingrese los Apellidos del Trabajador: ")
+        Profesion = raw_input("Ingrese la Profesion del Trabajador: ")
+        Telefono = raw_input("Ingrese el Telefono del Trabajador: ")
+        Emp.Alta(Numero,Nombre,Apellidos,Profesion,Telefono)
     
