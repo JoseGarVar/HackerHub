4992db2e208149c3ac9b5672b322f90ef168ec27
José García V <jose3500@gmail.com>
2012-02-03 12:35:31 -0600

    Alta casi Lista, Falta escirbir en un archivo

--- Python_HH/ABCM_Python/Empleado.py
+++ Python_HH/ABCM_Python/Empleado.py
@@ -1,5 +1,9 @@
+Lista = []
+
 class Empleado:
     #Abstraccion de los Objetos Empleado
+    
+    
     def __init__(self):
         print "Accediendo A la Base de Datos"
         
@@ -16,6 +20,7 @@ class Empleado:
         self.Github = Github
         self.Twitter = Twitter
         self.Facebook = Facebook
+        return True
         
     def Proyecto(self,NombreP = "",Tipo = "",Tecnologia = "",Repositorio = "",Sitio = "",Contribudores = ""):
         self.NombreP = NombreP
@@ -24,14 +29,15 @@ class Empleado:
         self.Repositorio = Repositorio
         self.Sitio = Sitio
         self.Contribudores = Contribudores
+        return True
         
     def Imprimir_Personal(self):
         print "Numero de Empleado" , self.Numero
         print "Nombre:" , self.Nombre , self.Apellidos
         print "Profesion/Ocupacion:" , self.Profesion
-        print "Telefono:" ,int(self.Telefono)
+        print "Telefono:" ,self.Telefono
         
-    def Imprimir_Gustos(self):
+    def Imprimir_Redes(self):
         print "Github:" , self.Github
         print "Twitter:" , self.Twitter
         print "Facebook:" , self.Facebook
@@ -44,12 +50,29 @@ class Empleado:
         print "Sitio:" , self.Sitio
         print "Contribudores:" , self.Contribudores 
         
-    def Alta(self,Numero,Nombre,Apellidos,Profesion,Telefono):
-        if self.Personal(Numero,Nombre,Apellidos,Profesion,Telefono) == True:
-            print "Dado de Alta..."
-        self.Imprimir_Personal()
+    def Alta(self,Numero,Nombre,Apellidos,Profesion,Telefono,Github,Twitter,Facebook):
+        if self.Personal(Numero,Nombre,Apellidos,Profesion,Telefono) == True and self.Redes(Github,Twitter,Facebook) == True:
+            print "---------------------------------"
+            print "Alta Satisfactoria\n"
+            print "---------------------------------"
+            self.Imprimir_Personal()
+            self.Imprimir_Redes()
+            self.Agregar_Lista(Numero,Nombre,Apellidos,Profesion,Telefono,Github,Twitter,Facebook)
+            print Lista
+            
+    def Agregar_Lista(self,Numero,Nombre,Apellidos,Profesion,Telefono,Github,Twitter,Facebook):
+        Lista.append(Numero)
+        Lista.append(Nombre)
+        Lista.append(Apellidos)
+        Lista.append(Profesion)
+        Lista.append(Telefono)
+        Lista.append(Github)
+        Lista.append(Twitter)
+        Lista.append(Facebook)
+        
  
  
  #Falta crear funciones alta, baja, etc       
         
 #Guardar con una , y cuando se lea se divide
+
--- Python_HH/ABCM_Python/main.py
+++ Python_HH/ABCM_Python/main.py
@@ -1,20 +1,19 @@
 import Empleado
 
 MAX_EMP = 100
-Numero = 1
+Numero_Empleados = 0
 Emp = Empleado.Empleado()
-op = 1
+op = 6
 
-while(op <> 5):
-    
-    print "Seleccione una Opcion:\n"
+while(op > 5 or op < 1):
+
     print "---------------------------------"
     print "1.- Alta de Empleado"
     print "2.- Baja de Empleado"
     print "3.- Consulta de Empleado"
     print "4.- Modificar Empleado"
     print "5.- SALIR"
-    op = int(raw_input(": "))
+    op = int(raw_input("Seleccione una Opcion: "))
     print "---------------------------------"
 
     #Opcion = { 1 : "Alta de Empleado" , 2 : "Baja de Empleado" , 3 : "Consulta de Empleado" , 4 : "Modificar Empleado"}
@@ -27,11 +26,63 @@ while(op <> 5):
 
 
 #Altas
-    if op == 1:    
-        Numero = raw_input("Ingrese el Numero del Trabajador: ")
+    if op == 1:   
+        Numero_Empleados += 1
+        Numero = Numero_Empleados 
         Nombre = raw_input("Ingrese el Nombre del Trabajador: ")
         Apellidos = raw_input("Ingrese los Apellidos del Trabajador: ")
         Profesion = raw_input("Ingrese la Profesion del Trabajador: ")
         Telefono = raw_input("Ingrese el Telefono del Trabajador: ")
-        Emp.Alta(Numero,Nombre,Apellidos,Profesion,Telefono)
+        
+        Mal = 1
+        Red = "No"
+        Proy = "NO"
+        
+        while Mal == 1:
+            
+            Red = raw_input("Usted cuenta con GitHub (Responda SI/NO):")        
+            if Red == "NO":
+                Github = "NO"
+                Mal = 0
+            elif Red <> "NO" and not Red == "SI" or Red <> "SI":
+                print "Responda en Mayusculas SI o NO"
+                Mal == 1
+            elif Red == "SI":
+                Github = raw_input("\nDigite su cuenta GitHub: ")
+                Mal = 0
+        
+        Mal = 1        
+        
+        while Mal == 1:
+            
+            Red = raw_input("Usted cuenta con Twitter (Responda SI/NO):")        
+            if Red == "NO":
+                Twitter = "NO"
+                Mal = 0
+            elif Red <> "NO" and not Red == "SI" or Red <> "SI":
+                print "Responda en Mayusculas SI o NO"
+                Mal == 1
+            elif Red == "SI":
+                Twitter = raw_input("\nDigite su cuenta Twitter: ")
+                Mal = 0
+    
+        Mal = 1
+          
+        while Mal == 1:
+            
+            Red = raw_input("Usted cuenta con Facebook (Responda SI/NO):")        
+            if Red == "NO":
+                Facebook = "NO"
+                Mal = 0
+            elif Red <> "NO" and not Red == "SI" or Red <> "SI":
+                print "Responda en Mayusculas SI o NO"
+                Mal == 1
+            elif Red == "SI":
+                Facebook = raw_input("\nDigite su cuenta Facebook: ")
+                Mal = 0                
+            
+        Emp.Alta(Numero,Nombre,Apellidos,Profesion,Telefono,Github,Twitter,Facebook)
+    
+        op = 6 #Para volver a cargar el menu
+        
     
